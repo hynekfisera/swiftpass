9539495dbd5336f86f33a06e132a78fd2f510e4c
Hynek Fišera <53520774+hynekfisera@users.noreply.github.com>
2021-12-04 18:44:47 +0100

    Remember page done

--- pages/remember.js
+++ pages/remember.js
@@ -7,6 +7,21 @@ export default function Remember() {
   const [input, setInput] = useState("");
   const [info, setInfo] = useState("Enter a password");
 
+  function suffix(i) {
+    var j = i % 10,
+      k = i % 100;
+    if (j == 1 && k != 11) {
+      return i + "st";
+    }
+    if (j == 2 && k != 12) {
+      return i + "nd";
+    }
+    if (j == 3 && k != 13) {
+      return i + "rd";
+    }
+    return i + "th";
+  }
+
   const onEnter = () => {
     if (input !== "") {
       if (step === 0) {
@@ -22,15 +37,23 @@ export default function Remember() {
   };
 
   const onPrev = () => {
-    setInput("");
     if (step % 2 !== 0) {
-    } else {
-      if (step > 2) {
+      if (password.slice(0, Math.floor(step / 2)) !== input) {
         setStep(step - 2);
+        setInfo(`The ${suffix(Math.floor(step / 2) + 1)} character is ${password[Math.floor(step / 2)]}`);
       } else {
         setStep(step - 1);
       }
+      setInput("");
+    } else {
+      setInput("");
+      setStep(step - 1);
     }
+  };
+
+  const onTryAgain = () => {
+    setInput("");
+    setStep(step - 1);
   };
 
   const onReset = () => {
@@ -50,12 +73,12 @@ export default function Remember() {
           setInfo("Correct! Now you remember the password!");
         } else {
           if (step === 1) {
-            setInfo(`The ${character + 1} character is ${password[character]}`);
+            setInfo(`The ${character + 1}st character is ${password[character]}`);
           } else {
             if (password.slice(0, character) === input) {
-              setInfo(`Correct! The ${character + 1} character is ${password[character]}`);
+              setInfo(`Correct! The ${suffix(character + 1)} character is ${password[character]}`);
             } else {
-              setInfo("Wrong! Go back or try again");
+              setInfo("Wrong! Try again or go back");
             }
           }
         }
@@ -90,9 +113,13 @@ export default function Remember() {
                 <button onClick={onEnter} className="btn btn-l btn-secondary">
                   Enter
                 </button>
-              ) : (
+              ) : step === 1 || password.slice(0, Math.floor(step / 2)) === input ? (
                 <button onClick={onNext} className="btn btn-l btn-primary">
                   Next
+                </button>
+              ) : (
+                <button onClick={onTryAgain} className="btn btn-l btn-primary">
+                  Try again
                 </button>
               )
             ) : (
@@ -101,7 +128,7 @@ export default function Remember() {
               </button>
             )}
           </div>
-          {step > 1 && <span onClick={onPrev}>Go back</span>}
+          {step > 0 && (step > 1 ? <span onClick={onPrev}>Go back</span> : <span onClick={onReset}>Go back</span>)}
         </div>
       </main>
     </>
